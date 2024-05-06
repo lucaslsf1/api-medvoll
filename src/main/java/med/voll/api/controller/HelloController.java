cca92dc7253d9c29e2e08dfcd09ede3ae346b294
Lucas <lucasfiguero2000@gmail.com>
2024-05-06 14:17:10 -0300

    Iniciando projeto

---
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,20 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String inicio(){
+        return "Página inicial da api.";
+    }
+
+    @GetMapping("/hello")
+    public String olaMundo() {
+        return "Olá, Mundo!";
+    }
+}
